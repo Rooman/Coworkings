d01a2c3832c3a8bc05f37c4ac0c81410e60c2385
nomar <nomarchia2@gmail.com>
2020-01-30 18:14:40 +0200

    Remove unnecessary User setters

--- src/main/java/com/javastudy/coworkings/entity/User.java
+++ src/main/java/com/javastudy/coworkings/entity/User.java
@@ -18,72 +18,36 @@ public class User {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getFullName() {
         return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     public UserRole getRole() {
         return role;
     }
 
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
     public String getTel() {
         return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getSole() {
         return sole;
-    }
-
-    public void setSole(String sole) {
-        this.sole = sole;
     }
 
     public static UserBuilder newBuilder() {
