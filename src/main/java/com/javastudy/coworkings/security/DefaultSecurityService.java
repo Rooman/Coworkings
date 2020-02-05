22df75c0e6b74b67dd641de3ddbe69711992e6f3
nomar <nomarchia2@gmail.com>
2020-02-05 16:40:51 +0200

    Initial Commit (created classes needed to implement)

---
+++ src/main/java/com/javastudy/coworkings/security/DefaultSecurityService.java
@@ -0,0 +1,4 @@
+package com.javastudy.coworkings.security;
+
+public class DefaultSecurityService {
+}
---
+++ src/main/java/com/javastudy/coworkings/web/filter/UserSecurityFilter.java
@@ -0,0 +1,4 @@
+package com.javastudy.coworkings.web.filter;
+
+public class UserSecurityFilter {
+}
