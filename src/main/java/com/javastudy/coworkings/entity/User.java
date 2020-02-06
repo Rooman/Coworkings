a30924a93ef08bae11994456c4ff09d145a04efd
miari <maryna.boroday@gmail.com>
2020-02-06 16:09:58 +0100

    @Getter, @Builder, @NoArgsConstructor, @AllArgsConstructor Lombok annotations were added to Coworking and User classes; role was changed to UserRole, getRole to get UserRole in User class, UserMapperTest class, JdbcUserDaoTest class; User creation in classes UserMapper and JdbcUserDaoTest were reworked to correspond @Builder annotation of Lombok; User creation in classes CoworkingRowMapper and CoworkingBuilderTest were reworked to correspond @Builder annotation of Lombok; pom file was updated with Lombok dependency

--- src/main/java/com/javastudy/coworkings/entity/User.java
+++ src/main/java/com/javastudy/coworkings/entity/User.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class User {
     private long id;
     private String username;
@@ -20,102 +19,4 @@ public class User {
     private String email;
     private String city;
     private String sole;
-
-    /*private User() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getSole() {
-        return sole;
-    }
-
-    public static UserBuilder newBuilder() {
-        return new User().new UserBuilder();
-    }
-
-    public class UserBuilder {
-
-        private UserBuilder() {
-        }
-
-        public UserBuilder setId(Long id) {
-            User.this.id = id;
-            return this;
-        }
-
-        public UserBuilder setUsername(String username) {
-            User.this.username = username;
-            return this;
-        }
-
-        public UserBuilder setPassword(String password) {
-            User.this.password = password;
-            return this;
-        }
-
-        public UserBuilder setFullName(String fullName) {
-            User.this.fullName = fullName;
-            return this;
-        }
-
-        public UserBuilder setUserRole(UserRole userRole) {
-            User.this.userRole = userRole;
-            return this;
-        }
-
-        public UserBuilder setTel(String tel) {
-            User.this.tel = tel;
-            return this;
-        }
-
-        public UserBuilder setEmail(String email) {
-            User.this.email = email;
-            return this;
-        }
-
-        public UserBuilder setCity(String city) {
-            User.this.city = city;
-            return this;
-        }
-
-        public UserBuilder setSole(String sole) {
-            User.this.sole = sole;
-            return this;
-        }
-
-        public User build() {
-            return User.this;
-        }
-    }*/
 }
